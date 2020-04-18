c7d84d5b933d7b02ca110fdd9f069b2d331ad629
NoussaOns <38370333+NoussaOns@users.noreply.github.com>
2020-04-18 11:41:03 +0400

    Update Worms.cpp

--- Worms.cpp
+++ Worms.cpp
@@ -1,36 +1,24 @@
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 int main(){
     int n;
     scanf("%d", &n);
-    int a[n], sum = 0;
-    bool chosen[n] = {0};
-
-    // for (int i=0;i<n;i++){
-    //     printf("%d", chosen[i]);
-        
-    // }
-    // printf("\n");
-
-    for (int i=0;i<n;i++){
-        scanf("%d", &a[i]);
-        sum += a[i];
+    int a[n];
+    a[0] = 1;
+    for(int i=1;i<n;++i){
+        int inp;
+        scanf("%d", &inp);
+        a[i] = a[i-1] + inp;
     }
-
-    int req = 2*sum / n;
-    for (int i=0;i<n;i++){
-        if (!chosen[i]){
-            
-            printf("%d ", i+1);
-            for (int j=i+1;j<n;j++){
-                if (!chosen[j] && req - a[i] == a[j]){
-                    chosen[j] = 1;
-                    printf("%d\n", j+1);
-                    break;
-                }
-            }
-            chosen[i] = 1;
-        }
+    int m,k,j;
+    scanf("%d", &m);
+    scanf("%d", &m);
+    for(int i=0;i<m;i++){
+        scanf("%d", &k);
+        j = lower_bound(a,a+n,k) - a;
+        if (a[j] == k) printf("%d\n", j+1);
+        else printf("%d\n", j);
     }
 }
